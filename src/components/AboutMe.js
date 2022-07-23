03774b5901839a9065480859c6e08a722c2532d2
danna <dannavila.1809@gmail.com>
2022-07-23 16:37:57 -0500

    Update and Style Intro and AboutMe components

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,43 +4,45 @@ import {ReactComponent as TechCSS} from '../img/css.svg';
 import {ReactComponent as TechJS} from '../img/js.svg';
 import {ReactComponent as TechREACT} from '../img/react.svg';
 import {ReactComponent as TechGIT} from '../img/git.svg';
+import "../css/AboutMe.css";
+
 function AboutMe() {
     const technologiesItems ={
         htm5:{
-            img:<TechHTML/>,
+            img:<TechHTML className="tech-icon"/>,
             tech:"HTML5"
         },
         css:{
-            img:<TechCSS/>,
+            img:<TechCSS className="tech-icon"/>,
             tech: "CSS"
         },
         js:{
-            img:<TechJS/>,
+            img:<TechJS className="tech-icon"/>,
             tech: "JavaScript"
         },
         react:{
-            img:<TechREACT/>,
+            img:<TechREACT className="tech-icon"/>,
             tech: "React.js"
         },
         git:{
-            img:<TechGIT/>,
+            img:<TechGIT className="tech-icon"/>,
             tech: "Git"
         }
     };
     return ( 
         <Section>
             <h2>about me .</h2>
-            <p>I am a fourth year <strong>software development student</strong> at the technological university of Peru. I am also a self-taught person. I have great interest in <strong>frontend</strong> and <strong>backend</strong> development.</p>
+            <p className="aboutme-description">I am a fourth year <strong>software development student</strong> at the technological university of Peru. I am also a self-taught person. I have great interest in <strong>frontend</strong> and <strong>backend</strong> development.</p>
             <h3>Technologies I’ve been working with:</h3>
             
-            <div>
+            <ul>
                 {Object.keys(technologiesItems).map((tech)=>(
-                    <div key={tech}>
+                    <li className="tech-list" key={tech}>
                         {technologiesItems[tech]["img"]}
-                        <p>{technologiesItems[tech]["tech"]}</p>
-                    </div>
+                        {technologiesItems[tech]["tech"]}
+                    </li>
                 ))}
-            </div>
+            </ul>
         </Section>
      );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,10 @@
+import "../css/Intro.css";
 function Intro() {
     return ( 
-        <header>
-           <p>Hi! I'm</p> 
-           <h1>danna.</h1>
-           <p>I am a very passionate software engineering student. I am interested in frontend and backend development.</p>
+        <header className="header">
+           <p className="intro-greet">Hi! I'm</p> 
+           <h1>Danna.</h1>
+           <p className="intro-description">I am a very passionate software engineering student. I am interested in frontend and backend development.</p>
         </header>
      );
 }
