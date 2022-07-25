d3d68a94301e65aa7392f1a56b45d20af7e92264
danna <dannavila.1809@gmail.com>
2022-07-24 20:25:39 -0500

    Update ProjectTab component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import AboutMe from './components/AboutMe';
 function App() {
   return (
     <Routes>
--- src/components/ProjectTab.js
+++ src/components/ProjectTab.js
@@ -6,37 +6,34 @@ import {default as ProjectGoodsleep} from '../img/goodsleep.png';
 function ProjectTab() {
 
     const [projectactive,setProjectActive] = useState("personal");
-    const  personal= [
-        {
-            title:"Danna Portfolio",
-            techs:"html5/css/React.js",
-            description:"My personal portfolio developed with React.js from scratch and bootstrapped with Create React App",
-            img:<img className="project-img" src={ProjectPortfolio} alt="Bookmark"/>,
-            linkGitRepo:"https://github.com/dannxvc/danna-portfolio",
-            linkLiveSite:"https://bookmark-pg-react.pages.dev/"
-        },
-        {
-            title:"Bookmark Manager",
-            techs:"html5/css/React.js",
-            description:"A frontendmentor challenge.Website developed with React.js from scratch.",
-            img:<img className="project-img" src={ProjectBookmark} alt="Bookmark"/>,
-            linkGitRepo:"https://github.com/dannxvc/bookmark-pg-react",
-            linkLiveSite:"https://bookmark-pg-react.pages.dev/"
-        }
-    ];
-    const university= [
-        {
-            title: "Good Sleep Hotel",
-            techs: "html5/css/JavaScript/Java",
-            description: "A hotel application, client and administration interfaces, reservation function.",
-            img:<img className="project-img" src={ProjectGoodsleep} alt="Bookmark"/>,
-            linkGitRepo:"https://github.com/dannxvc/goodsleepproyecto",
-       },
-    ];
-
     const projectItems = {  
-        personal,
-        university
+        personal:[
+            {
+                title:"Danna Portfolio",
+                techs:"html5/css/React.js",
+                description:"My personal portfolio developed with React.js from scratch and bootstrapped with Create React App",
+                img:<img className="project-img" src={ProjectPortfolio} alt="Bookmark"/>,
+                linkGitRepo:"https://github.com/dannxvc/danna-portfolio",
+                linkLiveSite:"https://bookmark-pg-react.pages.dev/"
+            },
+            {
+                title:"Bookmark Manager",
+                techs:"html5/css/React.js",
+                description:"A frontendmentor challenge.Website developed with React.js from scratch.",
+                img:<img className="project-img" src={ProjectBookmark} alt="Bookmark"/>,
+                linkGitRepo:"https://github.com/dannxvc/bookmark-pg-react",
+                linkLiveSite:"https://bookmark-pg-react.pages.dev/"
+            }
+        ],
+        university: [
+            {
+                title: "Good Sleep Hotel",
+                techs: "html5/css/JavaScript/Java",
+                description: "A hotel application, client and administration interfaces, reservation function.",
+                img:<img className="project-img" src={ProjectGoodsleep} alt="Bookmark"/>,
+                linkGitRepo:"https://github.com/dannxvc/goodsleepproyecto",
+           },
+        ]
     };
     return ( 
         <div className="project">
